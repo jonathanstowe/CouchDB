8b2b9920761db27562399bf364b6b1d6b66c3636
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-11-24 11:05:26 +0000

    Fix up Config

--- lib/CouchDB/Config.pm
+++ lib/CouchDB/Config.pm
@@ -1,15 +1,16 @@
 use JSON::Class;
 use JSON::Name;
+
 class CouchDB::Config does JSON::Class {
-    class CouchDB::Config::Log does JSON::Class {
+    class Log does JSON::Class {
         has Str $.file;
         has Str $.include_sasl;
         has Str $.level;
     }
-    class CouchDB::Config::Cors does JSON::Class {
+    class Cors does JSON::Class {
         has Str $.credentials;
     }
-    class CouchDB::Config::CouchHttpdAuth does JSON::Class {
+    class CouchHttpdAuth does JSON::Class {
         has Str $.require_valid_user;
         has Str $.allow_persistent_cookies;
         has Str $.auth_cache_size;
@@ -18,7 +19,7 @@ class CouchDB::Config does JSON::Class {
         has Str $.iterations;
         has Str $.authentication_redirect;
     }
-    class CouchDB::Config::Daemons does JSON::Class {
+    class Daemons does JSON::Class {
         has Str $.auth_cache;
         has Str $.index_server;
         has Str $.stats_aggregator;
@@ -32,12 +33,12 @@ class CouchDB::Config does JSON::Class {
         has Str $.compaction_daemon;
         has Str $.vhosts;
     }
-    class CouchDB::Config::Ssl does JSON::Class {
+    class Ssl does JSON::Class {
         has Str $.port;
         has Str $.verify_ssl_certificates;
         has Str $.ssl_certificate_max_depth;
     }
-    class CouchDB::Config::HttpdDesignHandlers does JSON::Class {
+    class HttpdDesignHandlers does JSON::Class {
         has Str $._compact;
         has Str $._show;
         has Str $._info;
@@ -46,26 +47,26 @@ class CouchDB::Config does JSON::Class {
         has Str $._update;
         has Str $._rewrite;
     }
-    class CouchDB::Config::ViewCompaction does JSON::Class {
+    class ViewCompaction does JSON::Class {
         has Str $.keyvalue_buffer_size;
     }
-    class CouchDB::Config::Attachments does JSON::Class {
+    class Attachments does JSON::Class {
         has Str $.compressible_types;
         has Str $.compression_level;
     }
-    class CouchDB::Config::QueryServers does JSON::Class {
+    class QueryServers does JSON::Class {
         has Str $.javascript;
         has Str $.coffeescript;
     }
-    class CouchDB::Config::QueryServerConfig does JSON::Class {
+    class QueryServerConfig does JSON::Class {
         has Str $.os_process_limit;
         has Str $.reduce_limit;
     }
-    class CouchDB::Config::Stats does JSON::Class {
+    class Stats does JSON::Class {
         has Str $.samples;
         has Str $.rate;
     }
-    class CouchDB::Config::Couchdb does JSON::Class {
+    class Couchdb does JSON::Class {
         has Str $.plugin_dir;
         has Str $.uuid;
         has Str $.max_document_size;
@@ -79,11 +80,11 @@ class CouchDB::Config does JSON::Class {
         has Str $.os_process_timeout;
         has Str $.view_index_dir;
     }
-    class CouchDB::Config::DatabaseCompaction does JSON::Class {
+    class DatabaseCompaction does JSON::Class {
         has Str $.checkpoint_after;
         has Str $.doc_buffer_size;
     }
-    class CouchDB::Config::Httpd does JSON::Class {
+    class Httpd does JSON::Class {
         has Str $.bind_address;
         has Str $.port;
         has Str $.enable_cors;
@@ -95,10 +96,10 @@ class CouchDB::Config does JSON::Class {
         has Str $.log_max_chunk_size;
         has Str $.allow_jsonp;
     }
-    class CouchDB::Config::CouchHttpdOauth does JSON::Class {
+    class CouchHttpdOauth does JSON::Class {
         has Str $.use_users_db;
     }
-    class CouchDB::Config::Replicator does JSON::Class {
+    class Replicator does JSON::Class {
         has Str $.max_replication_retry_count;
         has Str $.worker_batch_size;
         has Str $.verify_ssl_certificates;
@@ -110,7 +111,7 @@ class CouchDB::Config does JSON::Class {
         has Str $.http_connections;
         has Str $.connection_timeout;
     }
-    class CouchDB::Config::HttpdGlobalHandlers does JSON::Class {
+    class HttpdGlobalHandlers does JSON::Class {
         has Str $._active_tasks;
         has Str $._replicate;
         has Str $._db_updates;
@@ -120,22 +121,22 @@ class CouchDB::Config does JSON::Class {
         has Str $._restart;
         has Str $._log;
         has Str $._uuids;
-        has Str $.httpdglobalhandlersfavicon.ico is json-name('favicon.ico');
+        has Str $.favicon is json-name('favicon.ico');
         has Str $._oauth;
-        has Str $.httpdglobalhandlers/ is json-name('/');
+        has Str $.root is json-name('/');
         has Str $._plugins;
         has Str $._session;
         has Str $._utils;
     }
-    class CouchDB::Config::Uuids does JSON::Class {
+    class Uuids does JSON::Class {
         has Str $.max_count;
         has Str $.algorithm;
     }
-    class CouchDB::Config::CompactionDaemon does JSON::Class {
+    class CompactionDaemon does JSON::Class {
         has Str $.check_interval;
         has Str $.min_file_size;
     }
-    class CouchDB::Config::HttpdDbHandlers does JSON::Class {
+    class HttpdDbHandlers does JSON::Class {
         has Str $._all_docs;
         has Str $._compact;
         has Str $._design;
@@ -143,30 +144,30 @@ class CouchDB::Config does JSON::Class {
         has Str $._temp_view;
         has Str $._view_cleanup;
     }
-    class CouchDB::Config::Vendor does JSON::Class {
+    class Vendor does JSON::Class {
         has Str $.name;
         has Str $.version;
     }
-    has CouchDB::Config::Cors $.cors;
-    has CouchDB::Config::Log $.log;
-    has CouchDB::Config::HttpdDesignHandlers $.httpd_design_handlers;
-    has CouchDB::Config::Ssl $.ssl;
-    has CouchDB::Config::Daemons $.daemons;
-    has CouchDB::Config::CouchHttpdAuth $.couch_httpd_auth;
-    has CouchDB::Config::Attachments $.attachments;
-    has CouchDB::Config::ViewCompaction $.view_compaction;
-    has CouchDB::Config::Stats $.stats;
-    has CouchDB::Config::QueryServerConfig $.query_server_config;
-    has CouchDB::Config::QueryServers $.query_servers;
-    has CouchDB::Config::HttpdGlobalHandlers $.httpd_global_handlers;
-    has CouchDB::Config::Replicator $.replicator;
-    has CouchDB::Config::CouchHttpdOauth $.couch_httpd_oauth;
-    has CouchDB::Config::Httpd $.httpd;
-    has CouchDB::Config::DatabaseCompaction $.database_compaction;
-    has CouchDB::Config::Couchdb $.couchdb;
-    has CouchDB::Config::Uuids $.uuids;
-    has CouchDB::Config::Vendor $.vendor;
-    has CouchDB::Config::HttpdDbHandlers $.httpd_db_handlers;
-    has CouchDB::Config::CompactionDaemon $.compaction_daemon;
+    has Cors $.cors;
+    has Log $.log;
+    has HttpdDesignHandlers $.httpd_design_handlers;
+    has Ssl $.ssl;
+    has Daemons $.daemons;
+    has CouchHttpdAuth $.couch_httpd_auth;
+    has Attachments $.attachments;
+    has ViewCompaction $.view_compaction;
+    has Stats $.stats;
+    has QueryServerConfig $.query_server_config;
+    has QueryServers $.query_servers;
+    has HttpdGlobalHandlers $.httpd_global_handlers;
+    has Replicator $.replicator;
+    has CouchHttpdOauth $.couch_httpd_oauth;
+    has Httpd $.httpd;
+    has DatabaseCompaction $.database_compaction;
+    has Couchdb $.couchdb;
+    has Uuids $.uuids;
+    has Vendor $.vendor;
+    has HttpdDbHandlers $.httpd_db_handlers;
+    has CompactionDaemon $.compaction_daemon;
 }
 # vim: expandtab shiftwidth=4 ft=perl6
