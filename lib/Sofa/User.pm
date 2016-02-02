f935c3e0ed166f1edc1600aa4e52fac864f212f1
Jonathan Stowe <jns+git@gellyfish.co.uk>
2016-02-02 21:05:29 +0000

    Add a user class

---
+++ lib/Sofa/User.pm
@@ -0,0 +1,30 @@
+use v6.c;
+
+use JSON::Name;
+use JSON::Class;
+use Sofa::Document::Wrapper;
+
+class Sofa::User does JSON::Class does Sofa::Document::Wrapper {
+    has Str $.name;
+    has Str $.password          is json-skip-null;
+    has Str $.salt              is json-skip-null;
+    has Str $.derived_key       is json-skip-null;
+    has Int $.iterations        is json-skip-null;
+    has Str $.type          =   'user';
+    has Str $.password_scheme   is json-skip-null;
+    has Str @.roles;
+
+    method to-json() {
+        self.Sofa::Document::Wrapper::to-json();
+    }
+
+    method generate-id() returns Str {
+        my Str $s;
+        if $!name.defined {
+            $s = "org.couchdb.user:%s".sprintf($!name);
+        }
+        $s;
+    }
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6
