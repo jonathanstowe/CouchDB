790a22bf50ad0879d57b6492c5327e5c48b04e04
Jonathan Stowe <jns@gellyfish.co.uk>
2016-04-20 18:50:40 +0100

    Move the exception stuff to its own file

--- lib/Sofa/Database.pm
+++ lib/Sofa/Database.pm
@@ -4,7 +4,9 @@ use Sofa::Method;
 use JSON::Class:ver(v0.0.5..*);
 use Sofa::UserAgent;
 
-class Sofa::Database does JSON::Class {
+use Sofa::Exception;
+
+class Sofa::Database does JSON::Class does Sofa::Exception::Handler {
     use Sofa::Document;
     use Sofa::Document::Wrapper;
     use Sofa::Document::All;
@@ -35,56 +37,6 @@ class Sofa::Database does JSON::Class {
          [ flat $!name, %parts<path>.flat.grep({.defined}) ];
     }
 
-    class X::InvalidName is Exception {
-        has $.name;
-        method message() returns Str {
-            "'{$!name}' is not a valid DB name";
-        }
-    }
-
-    class X::DatabaseExists is Exception {
-        has $.name;
-        method message() returns Str {
-            "Database '{$!name}' already exists";
-        }
-    }
-
-    class X::DocumentConflict is Exception {
-        has $.name;
-        has $.what;
-        method message() returns Str {
-            "There was a conflict while { $!what } document '{$!name}'";
-        }
-    }
-    class X::NoDatabase is Exception {
-        has $.name;
-        method message() returns Str {
-            "Database '{$!name}' does not exist";
-        }
-    }
-
-    class X::NoDocument is Exception {
-        has $.name;
-        has $.what;
-        method message() {
-            "Document '{ $!name }' not found while '{ $!what }'";
-        }
-    }
-
-    class X::NotAuthorised is Exception {
-        has $.name;
-        has $.what;
-        method message() returns Str {
-            "You are not authorised to { $!what } database '{$!name}'";
-        }
-    }
-
-    class X::NoIdOrName is Exception {
-        has $.message = "Cannot put a design document without a name or id";
-    }
-
-
-
     method is-valid-name(Str:D $name) {
 	    my token valid-db-name {
 		    ^<[a .. z]><[a .. z0 .. 9_$()+/-]>*$
@@ -92,38 +44,6 @@ class Sofa::Database does JSON::Class {
         so ($name ~~ /<valid-db-name>/);
     }
 
-    enum ExceptionContext <Database Document>;
-
-    method !get-exception(Int() $code, $name, Str $what, ExceptionContext $context = Database) {
-        given $code {
-            when 400 {
-                X::InvalidName.new(:$name);
-            }
-            when 401 {
-                X::NotAuthorised.new(:$name, :$what);
-            }
-            when 404 {
-                given $context {
-                    when Database {
-                        X::NoDatabase.new(:$name);
-                    }
-                    when Document {
-                        X::NoDocument.new(:$name, :$what);
-                    }
-                }
-            }
-            when 409 {
-                X::DocumentConflict.new(:$name, :$what);
-            }
-            when 412 {
-                # This is not actually right as 412 is more context sensitive
-                X::DatabaseExists.new(:$name);
-            }
-            default {
-                die "WTF : $_";
-            }
-        }
-    }
 
     method fetch(Sofa::Database:U: Str :$name!, Sofa::UserAgent :$ua!) returns Sofa::Database {
         my $db;
@@ -632,7 +552,7 @@ class Sofa::Database does JSON::Class {
             }
         }
         else {
-            self!get-exception($response.code, $doc-id.grep({.defined}).join('/'), $what, Document).throw;
+            self!get-exception($response.code, $doc-id.grep({.defined}).join('/'), $what, Sofa::Exception::Document).throw;
         }
     }
 
--- lib/Sofa/Exception.pm
+++ lib/Sofa/Exception.pm
@@ -1,7 +1,95 @@
-use v6;
+use v6.c;
 
-class Sofa::X is Exception {
+class Sofa::Exception {
 
+    class X::InvalidName is Exception {
+        has $.name;
+        method message() returns Str {
+            "'{$!name}' is not a valid DB name";
+        }
+    }
+
+    class X::DatabaseExists is Exception {
+        has $.name;
+        method message() returns Str {
+            "Database '{$!name}' already exists";
+        }
+    }
+
+    class X::DocumentConflict is Exception {
+        has $.name;
+        has $.what;
+        method message() returns Str {
+            "There was a conflict while { $!what } document '{$!name}'";
+        }
+    }
+
+    class X::NoDatabase is Exception {
+        has $.name;
+        method message() returns Str {
+            "Database '{$!name}' does not exist";
+        }
+    }
+
+    class X::NoDocument is Exception {
+        has $.name;
+        has $.what;
+        method message() {
+            "Document '{ $!name }' not found while '{ $!what }'";
+        }
+    }
+
+    class X::NotAuthorised is Exception {
+        has $.name;
+        has $.what;
+        method message() returns Str {
+            "You are not authorised to { $!what } database '{$!name}'";
+        }
+    }
+
+    class X::NoIdOrName is Exception {
+        has $.message = "Cannot put a design document without a name or id";
+    }
+
+    class X::SofaWTF is Exception {
+        has $.message = "Unanticipated response";
+    }
+
+
+    enum ExceptionContext <Server Database Document>;
+
+    role Handler {
+        method !get-exception(Int() $code, $name, Str $what, ExceptionContext $context = Database) {
+            given $code {
+                when 400 {
+                    X::InvalidName.new(:$name);
+                }
+                when 401 {
+                    X::NotAuthorised.new(:$name, :$what);
+                }
+                when 404 {
+                    given $context {
+                        when Database {
+                            X::NoDatabase.new(:$name);
+                        }
+                        when Document {
+                            X::NoDocument.new(:$name, :$what);
+                        }
+                    }
+                }
+                when 409 {
+                    X::DocumentConflict.new(:$name, :$what);
+                }
+                when 412 {
+                    # This is not actually right as 412 is more context sensitive
+                    X::DatabaseExists.new(:$name);
+                }
+                default {
+                    X::SofaWTF.new;
+                }
+            }
+        }
+    }
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
